e6e5fd2e2d4ec3774db954be84afb53ca1617848
Rbrar5738 <rbrar5738@conestogac.on.ca>
2024-02-16 10:56:39 -0600

    Error handling in io
    Error handling in io

--- index.js
+++ index.js
@@ -14,17 +14,31 @@ app.get("/",(req,res)=>
     res.sendFile(fileName,options);
 });
 
-let users=0
-var newIo=io.of('/custom');
-newIo.on("connection",(socket)=>{
-    console.log("User Connected");
+/***************Rooms***************** */
+let roomno=1;
+let full=0;
+io.on("connection",(socket)=>{
+    socket.join(`Room-1${roomno}`);
+    io.sockets.in(`Room-1${roomno}`).emit("welcome",`You are conneted to Room no ${roomno}`);
+    full++;
+    if(full >= 2)
+     {
+        full=0;
+        roomno++;
+    }
+});
+
+// let users=0
+// var newIo=io.of('/custom');
+// newIo.on("connection",(socket)=>{
+//     console.log("User Connected");
     // users++;
 
     /**************io.sockets.emit is used to send message to all
     // io.sockets.emit("numberOfUsers",`Number of users connected are ${users}`);
 
     /****************socket is used to send new user only*************/
-    socket.emit("welcome","Welcome to chat");
+    // socket.emit("welcome","Welcome to chat");
 
     //This is for that users who are already connected
     // socket.broadcast.emit("numberOfUsers",`Number of users connected are ${users}`);
@@ -43,7 +57,7 @@ newIo.on("connection",(socket)=>{
     // });
 
     
-});
+// });
 
 http.listen(port,()=>{
     console.log(`Server started at port ${port}`);
