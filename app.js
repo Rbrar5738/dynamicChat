7d44a4e7675d1f97f39d94ed0a55bc86e26b6dea
Rbrar5738 <rbrar5738@conestogac.on.ca>
2024-02-29 11:53:45 -0600

    User model created

--- app.js
+++ app.js
@@ -2,4 +2,14 @@ const dotenv = require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/chat");
+mongoose.connect("mongodb://127.0.0.1:27017/chatApp");
+
+const express = require("express");
+
+const app = express();
+
+const http = require("http").Server(app);
+const PORT = 8000;
+http.listen(PORT, () => {
+  console.log(`Server started att Port ${PORT}`);
+});
