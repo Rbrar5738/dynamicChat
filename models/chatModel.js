74bafeef3b0cff7854806a582d6b07ed75f9cf81
Rbrar5738 <rbrar5738@conestogac.on.ca>
2024-02-29 12:00:23 -0600

    Chat Schema Added

---
+++ models/chatModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema(
+  {
+    sender_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    reciver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: strue }
+);
+
+const userModel = mongoose.model("chat", userSchema);
+
+module.exports = {
+  userModel,
+};
