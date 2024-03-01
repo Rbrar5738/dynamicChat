33a8a5c13400c6b3924936bc79b8af6c26c3fbd4
Rbrar5738 <rbrar5738@conestogac.on.ca>
2024-03-01 09:58:01 -0600

    sockets

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const io = require("socket.io")(http);
 
 const bodyParser = require("body-parser");
 const { userRoute } = require("./routes/userRoute");
 const http = require("http").Server(app);
-const PORT = 8000;
 
+const io = require("socket.io")(http);
+const usp = io.of("/user-namescpace");
+
+usp.on("connection", (socket) => {
+  console.log("User Connected");
+
+  socket.on("disconnect", () => {
+    console.log("User Disonnected");
+  });
+});
+
+const PORT = 8000;
 mongoose.connect("mongodb://127.0.0.1:27017/chatApp");
 
 app.set("view engine", "ejs");
